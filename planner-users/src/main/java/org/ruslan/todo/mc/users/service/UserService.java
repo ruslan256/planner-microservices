01cbba99194fab11a283ad3ef711a829792815fb
ruslan256 <github.rus.yan@mail.ru>
2022-06-30 01:48:51 +0800

    feat(service): add service for 'User' class

---
+++ planner-users/src/main/java/org/ruslan/todo/mc/users/service/UserService.java
@@ -0,0 +1,49 @@
+package org.ruslan.todo.mc.users.service;
+
+import org.ruslan.todo.mc.entity.User;
+import org.ruslan.todo.mc.users.repo.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    public User add(User user) {
+        return repository.save(user);
+    }
+
+    public User update(User user) {
+        return repository.save(user);
+    }
+
+    public void deleteByUserId(Long id) {
+        repository.deleteById(id);
+    }
+
+    public void deleteByUserEmail(String email) {
+        repository.deleteByEmail(email);
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public Page<User> findByParams(String email, String username, PageRequest paging) {
+        return repository.findByParams(email, username, paging);
+    }
+
+}
