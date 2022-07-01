2657a4e4a5707cf66997291292bce36ad390bbad
ruslan256 <github.rus.yan@mail.ru>
2022-07-01 23:54:36 +0800

    fix(planner-users): add annotation @EntityScan into Main class

--- planner-users/src/main/java/org/ruslan/todo/mc/users/PlannerUsersApplication.java
+++ planner-users/src/main/java/org/ruslan/todo/mc/users/PlannerUsersApplication.java
@@ -2,12 +2,16 @@ package org.ruslan.todo.mc.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = {"org.ruslan.todo.mc"})
 @EnableJpaRepositories(basePackages = {"org.ruslan.todo.mc.users.repo"})
+@EntityScan(basePackages = {"org.ruslan.todo.mc.entity"})
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
