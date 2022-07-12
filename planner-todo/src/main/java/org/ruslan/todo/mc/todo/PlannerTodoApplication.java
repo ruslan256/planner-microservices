9a5e80614ba52961ef893600f71bdabbff5d45b3
ruslan256 <github.rus.yan@mail.ru>
2022-07-12 23:57:18 +0800

    feat(planner-todo): add interface for FeignClient

--- planner-todo/src/main/java/org/ruslan/todo/mc/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/org/ruslan/todo/mc/todo/PlannerTodoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"org.ruslan.todo.mc"})
 @EnableJpaRepositories(basePackages = {"org.ruslan.todo.mc.todo.repo"})
 @EntityScan(basePackages = {"org.ruslan.todo.mc.entity"})
+@EnableFeignClients
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
