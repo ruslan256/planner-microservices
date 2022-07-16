95197dd3bac62fc4f17fb2dfa2c0e0302346e387
ruslan256 <github.rus.yan@mail.ru>
2022-07-16 19:46:24 +0800

    feat(todo/config): add priority override for configuration files in the 'planner-todo' module

---
+++ planner-todo/src/main/java/org/ruslan/todo/mc/todo/config/PropertySourcesPlaceholderConfig.java
@@ -0,0 +1,31 @@
+package org.ruslan.todo.mc.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class PropertySourcesPlaceholderConfig {
+
+    private static final String NAME_PROPERTIES_FILE = "application-local.properties";
+
+    public PropertySourcesPlaceholderConfig() {
+        super();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+        Resource[] resources = new ClassPathResource[]{ new ClassPathResource(NAME_PROPERTIES_FILE) };
+        pspc.setLocations(resources);
+        pspc.setIgnoreResourceNotFound(true);
+        pspc.setLocalOverride(true);
+        pspc.setIgnoreUnresolvablePlaceholders(true);
+        pspc.setTrimValues(true);
+        pspc.setOrder(1);
+        return pspc;
+    }
+
+}
