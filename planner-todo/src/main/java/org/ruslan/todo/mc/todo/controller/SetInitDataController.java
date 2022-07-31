d3c9ba6d8109aad3bc70f1a86e99334177622433
ruslan256 <github.rus.yan@mail.ru>
2022-08-01 00:26:07 +0800

    imp(todo): edit controller, calling a special service to initialize data

--- planner-todo/src/main/java/org/ruslan/todo/mc/todo/controller/SetInitDataController.java
+++ planner-todo/src/main/java/org/ruslan/todo/mc/todo/controller/SetInitDataController.java
@@ -1,96 +1,26 @@
 package org.ruslan.todo.mc.todo.controller;
 
-import org.ruslan.todo.mc.entity.Category;
-import org.ruslan.todo.mc.entity.Priority;
-import org.ruslan.todo.mc.entity.Task;
-import org.ruslan.todo.mc.todo.service.CategoryService;
-import org.ruslan.todo.mc.todo.service.PriorityService;
-import org.ruslan.todo.mc.todo.service.TaskService;
+import org.ruslan.todo.mc.todo.service.UserInitDataService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Calendar;
-import java.util.Date;
-
 @RestController
 @RequestMapping("/data")
 public class SetInitDataController {
 
-    private final TaskService taskService;
-    private final PriorityService priorityService;
-    private final CategoryService categoryService;
+    private final UserInitDataService userInitDataService;
 
-    public SetInitDataController(TaskService taskService, PriorityService priorityService, CategoryService categoryService) {
-        this.taskService = taskService;
-        this.priorityService = priorityService;
-        this.categoryService = categoryService;
+    public SetInitDataController(UserInitDataService userInitDataService) {
+        this.userInitDataService = userInitDataService;
     }
 
     @PostMapping("/init")
     public ResponseEntity<Boolean> init(@RequestBody Long userId) {
 
-        // set priority
-        Priority prior1 = new Priority();
-        prior1.setColor("#fff");
-        prior1.setTitle("Важный");
-        prior1.setUserId(userId);
-
-        Priority prior2 = new Priority();
-        prior2.setColor("#ffе");
-        prior2.setTitle("Неважный");
-        prior2.setUserId(userId);
-
-        priorityService.add(prior1);
-        priorityService.add(prior2);
-
-        // set category
-        Category cat1 = new Category();
-        cat1.setTitle("Работа");
-        cat1.setUserId(userId);
-
-        Category cat2 = new Category();
-        cat2.setTitle("Семья");
-        cat2.setUserId(userId);
-
-        categoryService.add(cat1);
-        categoryService.add(cat2);
-
-        // set tomorrow
-        Date tomorrow = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(tomorrow);
-        c.add(Calendar.DATE, 1);
-        tomorrow = c.getTime();
-
-        // set one week
-        Date oneWeek = new Date();
-        Calendar c2 = Calendar.getInstance();
-        c2.setTime(oneWeek);
-        c2.add(Calendar.DATE, 7);
-        oneWeek = c2.getTime();
-
-        // set task
-        Task task1 = new Task();
-        task1.setTitle("Завершить 1-ый проект");
-        task1.setCategory(cat1);
-        task1.setPriority(prior1);
-        task1.setCompleted(true);
-        task1.setTaskDate(tomorrow);
-        task1.setUserId(userId);
-
-        Task task2 = new Task();
-        task2.setTitle("Поспать");
-        task2.setCategory(cat2);
-        task2.setCompleted(false);
-        task2.setPriority(prior2);
-        task2.setTaskDate(oneWeek);
-        task2.setUserId(userId);
-
-        taskService.add(task1);
-        taskService.add(task2);
+        userInitDataService.initUserData(userId);
 
         return ResponseEntity.ok(true);
     }
