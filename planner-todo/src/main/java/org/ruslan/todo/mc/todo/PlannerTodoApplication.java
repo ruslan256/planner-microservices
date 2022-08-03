0197a5327ed2dee4e509fc18a0806cbcb33f9638
ruslan256 <github.rus.yan@mail.ru>
2022-08-03 23:48:19 +0800

    imp(todo, users): add ability to dynamically modify configuration settings

--- planner-todo/src/main/java/org/ruslan/todo/mc/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/org/ruslan/todo/mc/todo/PlannerTodoApplication.java
@@ -3,6 +3,7 @@ package org.ruslan.todo.mc.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"org.ruslan.todo.mc.todo.repo"})
 @EntityScan(basePackages = {"org.ruslan.todo.mc.entity"})
 @EnableFeignClients
+@RefreshScope
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
--- planner-users/src/main/java/org/ruslan/todo/mc/users/PlannerUsersApplication.java
+++ planner-users/src/main/java/org/ruslan/todo/mc/users/PlannerUsersApplication.java
@@ -3,6 +3,7 @@ package org.ruslan.todo.mc.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"org.ruslan.todo.mc"})
 @EnableJpaRepositories(basePackages = {"org.ruslan.todo.mc.users.repo"})
 @EntityScan(basePackages = {"org.ruslan.todo.mc.entity"})
+@RefreshScope
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
