580b652355809da7155b471769a7c3d1996eed84
ruslan256 <github.rus.yan@mail.ru>
2022-08-07 00:10:57 +0800

    feat(users): add a message producer (reactor approach)

---
+++ planner-users/src/main/java/org/ruslan/todo/mc/users/mq/func/MessageActionsFunc.java
@@ -0,0 +1,26 @@
+package org.ruslan.todo.mc.users.mq.func;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Sinks;
+
+@Service
+@Getter
+@Slf4j
+public class MessageActionsFunc {
+
+    private final DefChannelFunc defChannelFunc;
+
+    public MessageActionsFunc(DefChannelFunc defChannelFunc) {
+        this.defChannelFunc = defChannelFunc;
+    }
+
+    // send a message
+    public void sendNewUserMessage(Long id) {
+        // add a new message to the listener
+        defChannelFunc.getInnerBus().emitNext(MessageBuilder.withPayload(id).build(), Sinks.EmitFailureHandler.FAIL_FAST);
+        log.info("Message sent: {}", id);
+    }
+}
